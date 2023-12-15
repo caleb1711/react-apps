63bf07836548969987883970b7065a4d592bafb1
caleb1711 <>
2023-12-15 21:55:34 +0000

    initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import FunctionalGreeting from './components/FunctionalGreeting';
+import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          <FunctionalGreeting />
+          <FunctionalGreetingWithProps />
+        </div>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+// function FunctionalGreeting(){
+//     return <h1>Hello from react!</h1>
+// }
+
+const FunctionalGreetingWithProps = () => <h1>What's up, my Gee!</h1>;
+
+export default FunctionalGreetingWithProps;
